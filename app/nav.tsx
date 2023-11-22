013bfeb9bf09829bfdb616235521e08b0787d95e
MabuelaThato <131166675+MabuelaThato@users.noreply.github.com>
2023-11-22 14:17:58 +0200

    update

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,23 +1,14 @@
 "use client"
 import React from 'react';
 import Link from 'next/link';
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-  navigationMenuTriggerStyle
-} from "@/components/ui/navigation-menu"
 
 const Navbar = () => {
   return (
     <div className='w-full shadow-md bg-[#F6F6F6] drop-shadow-lg'>
-        <div className='p-5 md:px-6 lg:px-20 items-center flex justify-between'>
-            <div><img src="/liberty.png" alt="" className='w-12 h-10 md:mr-8 lg:mr-10' /> </div>
+        <div className='p-5  px-6 lg:px-20 items-center flex justify-between'>
+            <div>
+              <img src="/liberty.png" alt="" className='w-14 h-14 md:w-20 md:h-16 md:mr-8 lg:mr-10' /> 
+              </div>
             <div className='flex flex-cols-3 gap-4 md:gap-6 lg:gap-9 mr-4 md:mr-8 lg:mr-10 '>
                 <Link href="/" className='hover:underline'>Home</Link>
                 <Link href="/services" className='hover:underline'>Services</Link>
@@ -25,30 +16,7 @@ const Navbar = () => {
               
             </div>
         </div>
-        {/* <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuItem>
-            <Link href="/" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Home
-              </NavigationMenuLink>
-            </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-            <Link href="/" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Se
-              </NavigationMenuLink>
-            </Link>
-            <Link href="/" legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Se
-              </NavigationMenuLink>
-            </Link>
-            </NavigationMenuItem>
-          </NavigationMenuList>
-        </NavigationMenu> 
-  */}
+        
     </div>
   )
 }
